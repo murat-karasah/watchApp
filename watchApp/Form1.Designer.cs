4fe27ad2b4a3aed179e1f0074de309591a33c589
mkara <mkara@DESKTOP-L2TJBTG>
2021-11-24 20:44:10 +0300

    Add project files.

---
+++ watchApp/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace watchApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblSaat = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDakika = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblSaniye = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnSifirla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblSaat
+            // 
+            this.lblSaat.AutoSize = true;
+            this.lblSaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSaat.Location = new System.Drawing.Point(45, 31);
+            this.lblSaat.Name = "lblSaat";
+            this.lblSaat.Size = new System.Drawing.Size(53, 37);
+            this.lblSaat.TabIndex = 0;
+            this.lblSaat.Text = "23";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(88, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = ":";
+            // 
+            // lblDakika
+            // 
+            this.lblDakika.AutoSize = true;
+            this.lblDakika.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDakika.Location = new System.Drawing.Point(102, 31);
+            this.lblDakika.Name = "lblDakika";
+            this.lblDakika.Size = new System.Drawing.Size(53, 37);
+            this.lblDakika.TabIndex = 2;
+            this.lblDakika.Text = "59";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(145, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 37);
+            this.label4.TabIndex = 3;
+            this.label4.Text = ":";
+            // 
+            // lblSaniye
+            // 
+            this.lblSaniye.AutoSize = true;
+            this.lblSaniye.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSaniye.Location = new System.Drawing.Point(158, 31);
+            this.lblSaniye.Name = "lblSaniye";
+            this.lblSaniye.Size = new System.Drawing.Size(53, 37);
+            this.lblSaniye.TabIndex = 4;
+            this.lblSaniye.Text = "59";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 71);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Başlat";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Enabled = false;
+            this.btnPause.Location = new System.Drawing.Point(109, 70);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 6;
+            this.btnPause.Text = "Durdur";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnSifirla
+            // 
+            this.btnSifirla.Enabled = false;
+            this.btnSifirla.Location = new System.Drawing.Point(200, 70);
+            this.btnSifirla.Name = "btnSifirla";
+            this.btnSifirla.Size = new System.Drawing.Size(75, 23);
+            this.btnSifirla.TabIndex = 7;
+            this.btnSifirla.Text = "Sıfırla";
+            this.btnSifirla.UseVisualStyleBackColor = true;
+            this.btnSifirla.Click += new System.EventHandler(this.Sıfırla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 113);
+            this.Controls.Add(this.btnSifirla);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblSaniye);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblDakika);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSaat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblSaat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDakika;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblSaniye;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnSifirla;
+    }
+}
+
---
+++ watchApp/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace watchApp
+{
+    public partial class Form1 : Form
+    {
+        public int dakika = 0, saniye=0,saat=0,sayac=0, NumberOfClick = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            lblSifirla();
+            timer1.Start();
+            btnPause.Enabled = true;
+            btnSifirla.Enabled = true;  
+            btnStart.Enabled = false;
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            ++NumberOfClick;
+            switch (NumberOfClick)
+            {
+                case 1:
+                    timer1.Stop();
+                    btnPause.Text = "Devam";
+                    break;
+                case 2:
+                    timer1.Start();
+                    btnPause.Text = "Durdur";
+                    NumberOfClick = 0;
+                    break;
+              
+                default:
+                   
+                    break;
+            }
+
+          
+          
+        }
+
+      
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            saniye++;
+            lblSaniye.Text = String.Format("{0:00}", saniye);
+            if (saniye>59)
+            {
+                lblSaniye.Text = String.Format("{0:00}", saniye);
+                saniye = 0;
+                dakika++;
+            }
+           else if (dakika>0)
+            {
+                lblDakika.Text = String.Format("{0:00}", dakika);
+                if (dakika>59)
+                {
+                    dakika = 0;
+                    saat++;
+                    lblSaat.Text = String.Format("{0:00}", saat);
+
+                }
+
+            }
+            else if (saat>23)
+            {
+                timer1.Stop();
+
+            }
+        }
+
+        private void Sıfırla_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer1.Dispose();
+            saniye=0;
+            dakika=0;
+            saat=0;
+            lblSifirla();
+            btnPause.Enabled = false;
+            btnSifirla.Enabled = false;
+            btnStart.Enabled = true;
+
+        }
+
+        private void lblSifirla()
+        {
+            btnPause.Text = "Durdur";
+            NumberOfClick = 0;
+            lblSaniye.Text = String.Format("{0:00}", saniye);
+            lblDakika.Text = String.Format("{0:00}", dakika);
+            lblSaat.Text = String.Format("{0:00}", saat);
+        }
+    }
+}
